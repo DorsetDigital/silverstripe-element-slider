f17d81b9b2b60ba8b26eb450be94dbde262623c3
Tim Burt <tim@dorset-digital.net>
2018-11-19 17:46:44 +0000

    Add tiny slider library

--- src/Controllers/SliderController.php
+++ src/Controllers/SliderController.php
@@ -8,6 +8,7 @@ class SliderController extends ElementController
 {
     public function init() {
         parent::init();
+        Requirements::javascript('dorsetdigital/silverstripe-element-slider:client/dist/tiny-slider/tiny-slider.js');
         Requirements::javascript('dorsetdigital/silverstripe-element-slider:client/dist/slider.min.js');
         Requirements::css('dorsetdigital/silverstripe-element-slider:client/dist/slider.css');
     }
